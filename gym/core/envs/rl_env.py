b3f78768be10eea391c0c81d66624076c447a64e
👾 👾 <xinyutian@deMacBook-Pro.local>
2023-02-25 22:57:12 +0800

    update rl_env

--- gym/core/envs/rl_env.py
+++ gym/core/envs/rl_env.py
@@ -1,18 +1,54 @@
 import gymnasium as gym
+from gymnasium import spaces
 
+def _get_obs(self):
+    return {"validators": self._strategies,
+            "proposing_order": self._proposing_order}
+
+def _get_info(self):
+    return {
+        "honest_proportion": sum(self._strategies == 0) / self.validator_size,
+    }
 
 class CustomEnv(gym.Env):
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """
         Initialize your custom environment.
 
         Parameters
         ----------
         """
+        # ENVIRONMENT: The validators
+        self.validator_size = 100 # The size of the validators
+        
+        # The strategies of the validators
+        self.strategy_space = spaces.Discrete(2) # The space of strategies: malicious v.s. honest
+        self._strategy_to_name = {0: "honest", 
+                                  1: "malicious"} # The mapping from strategy to name
+    
+        # The status of the validators
+        self.status_spaces = spaces.Discrete(2) # The space of statuses: propose, vote, committee
+        self._status_to_name = {0: "propose", 
+                                1: "vote"} # The mapping from status to name
+
+        # So every validator has a strategy and an status
+
+        # AGENT: The PoS Ethereum Blockchain, to learn the reward and penalty policies
+        self.action_space = spaces.Discrete(2)
+        self._action_to_name = {0: "reward",
+                                1: "penality"} # The mapping from action to name
+        
+        self.observation_space = spaces.Box(0, 1, shape=(2,))
+
+
+        self.window = None
+        self.clock = None
+
+
         super(CustomEnv, self).__init__()
         raise NotImplementedError
 
-    def reset(self):
+    def reset(self, seed = None, options = None):
         """
         Reset the environment and return an initial observation.
 
@@ -21,6 +57,26 @@ class CustomEnv(gym.Env):
         observation : numpy array
             The initial observation of the environment.
         """
+
+        # We need the following line to seed self.np_random
+        super().reset(seed = seed)
+
+        # Generate the strategies of the validators
+        self._strategies = self.np_random.randint(
+            0, self.strategy_space.n, size=self.validator_size
+        )
+
+        # Generate the proposing order
+        self._proposing_order = self.np_random.permutation(self.validator_size)
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, info
+
         raise NotImplementedError
 
     def step(self, action):
